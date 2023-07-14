b84ac0955e755a7c95810f522f515b75c578d217
Naman-14 <naman_14@outlook.com>
2023-07-14 12:36:04 +0530

    adding Modules

--- provider.tf
+++ provider.tf
@@ -7,9 +7,9 @@ terraform {
   }
 }
 
-# Configure the New Relic provider
-provider "newrelic" {
-  account_id = "3999429"
-  api_key = "NRAK-TZY5U3EGB7262LJA0RRYS7JYM41"    # Usually prefixed with 'NRAK'
-  region = "US"                    # Valid regions are US and EU
-}
+# # Configure the New Relic provider
+# provider "newrelic" {
+#   account_id = "3999429"
+#   api_key = "NRAK-TZY5U3EGB7262LJA0RRYS7JYM41"    # Usually prefixed with 'NRAK'
+#   region = "US"                    # Valid regions are US and EU
+# }
